119a4c87a63395ae43cf9d381f31b14c54d54149
vagrant <vagrant@vm3110-2018sp>
2018-03-23 15:58:42 -0400

    updated model, very basic

---
+++ model.mli
@@ -0,0 +1,20 @@
+type overworld_info = {
+
+}
+
+type combat_info = {
+
+}
+
+type person_info = {
+  id : string;
+  dollars : int;
+  location_id : string;
+
+}
+
+type game_info = {
+
+}
+
+type t
