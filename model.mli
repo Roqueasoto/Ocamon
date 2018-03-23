2f65c15bd3a9bf54c62c21318baa41365e5b9de8
Roque <ras656@cornell.edu>
2018-03-23 17:33:59 -0400

    Add some definition to types in model.mli

--- model.mli
+++ model.mli
@@ -1,9 +1,16 @@
 type overworld_info = {
-
+  mutable player_loc : int*int;
+  map_id : int;
+  mutable random_enc : bool;
+  mutable infront : object_info option;
 }
 
 type combat_info = {
-
+  turns : int;
+  party1 : pokemon_info list;
+  party2 : pokemon_info list;
+  items1 : item_info list;
+  items2 : item_info list;
 }
 
 type person_info = {
