0bbaf293fed491a01262c4eb203d966d2e22963b
yy545 <vagrant@vm3110-2018sp>
2018-03-23 15:39:43 -0400

    adding pokemon.mli

---
+++ pokemon.ml
@@ -0,0 +1,3 @@
+type ptype = Normal | Fire | Water | Electric | Grass | Ice | Fighting | Poison | Ground | Flying | Psychic
+                | Bug | Rock | Ghost | Dragon | Dark | Steel | Fairy
+type pokemon = {poketype : ptype; name : string; level : int}
