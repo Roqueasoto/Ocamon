cdfb8d47118e88f7fdace2c82d8d0da5821eb0bd
Roque <ras656@cornell.edu>
2018-04-23 17:03:25 -0400

    Finish rough implementation of AI and makes some edits to controller.mli

--- controller.mli
+++ controller.mli
@@ -1,20 +1,22 @@
 (* [effect] represents a combat effect on the game state. For type Switch, the
  * int carried represents the position of the Pokemon in the party that will be
- * swapped to the front. For all other types, Pokemon.pokemon indicated the
- * Pokemon being affected by the move. In these types, the second entry (int) in
+ * swapped to the front. For all other types, string indicates the Pokemon being
+ * affected by the move as self or other. In these types, the second entry (int)
  * the tuple designates the accuracy or likelihood of this effect and if this
- * effect is the first one in a CombatAction list, this is the accuracy of the
+ * in effect is the first one in a CombatAction list, this is the accuracy of
  * the move overall. For Damage and Heal, the third tuple (int) indicated the
  * amount of damage or healing performed. For Damage, the fourth tuple (a pair
  * of ints) indicated how many times the move is repeated (min, max).
- * Example - Ember's Damage - Bulbasaur , 100 , 30 , (1 , 1). *)
+ * Example - Ember's Damage - "other" , 100 , 30 , (1 , 1). The Nothing type
+ * is used for an action with no effect. *)
 type effect =
   | Switch of int
-  | Heal of Pokemon.pokemon    * int * int
-  | Damage of Pokemon.pokemon  * int * int * (int * int)
-  | Status of Pokemon.pokemon  * int
-  | Buff of Pokemon.pokemon    * int
-  | Special of Pokemon.pokemon * int
+  | Heal of string    * int * int
+  | Damage of string  * int * int * (int * int)
+  | Status of string  * int * string
+  | Buff of string    * int * string
+  | Special of string * int * string
+  | Nothing
 
 
 (* [command] represents a command input by a player. Parsed into one of the 7
@@ -24,8 +26,9 @@ type effect =
  * effect should be the main effect of the pokemon move.*)
 type command =
   | Move of string
-  | Interact 
+  | Interact
   | CombatAction of effect list
+  | Round of command * command
 
 (* [parse key] is the command that represents player input [key].
  * requires: [key] is a valid single keyboard input as described in the game's
