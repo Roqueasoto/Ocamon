298541a0634e915927c66f6a92683ce429bb970f
Andrea Yang <yy545@cornell.edu>
2018-04-25 19:45:35 -0400

    updated pokemon.mli

--- pokemon.mli
+++ pokemon.mli
@@ -20,14 +20,14 @@ val hp : t -> int
 (* [xp p] is the current xp value of the pokemon p. *)
 val xp : t -> int
 
-(* [atk p] is the current atk value of the pokemon p. *)
-val atk : t -> int
+(* [atk p] is the current atk value and stat stage of the pokemon p. *)
+val atk : t -> int * int 
 
-(* [def p] is the current def value of the pokemon p. *)
-val def : t -> int
+(* [def p] is the current def value and stat stage of the pokemon p. *)
+val def : t -> int * int
 
-(* [spd p] is the current spd value of the pokemon p. *)
-val spd : t -> int
+(* [spd p] is the current spd value and stat stage of the pokemon p. *)
+val spd : t -> int * int
 
 (* [maxhp p] is the maxiumum hp value of the pokemon p. *)
 val maxhp : t -> int
@@ -40,24 +40,30 @@ val rate_occ : t -> float
 
 (* [item_holding p] is the item the pokemon p is holding. None if the pokemon
  * isn't holding anything*)
-val item_holding : poke -> item option
+val item_holding : t -> item option
 
 (* [actions p] is the current list of CombatAction commands that the pokemon p
  * can perform. *)
-val actions : poke -> (int*Types.command) list
+val actions : t -> (int*Types.command) list
 
 (* [build_poke s] builds a pokemon of the name s from the json file j,
  * which contains info about all of the possible pokemons.
  * requires: s must be a valid name of a pokemon.*)
-val build_poke : string -> poke
+val build_poke : string -> t
 
 (* [random_poke] builds a random pokemon from the json file j,
  * which contains info about all of the possible pokemons.
  *)
-val random_poke : unit -> poke
+val random_poke : unit -> t
 
 (* [build_inventory] gives a random list of items*)
 val build_inventory : unit -> item list
+
+(* [pokemon_damage p e] process effect e on the pokemon p and returns a new pokemon *)
+val poke_effect : poke -> effect -> t
+
+(* Set all stat stages to zero*)
+val clear_buff : poke -> t
 
 (* [type_compare ptype1 ptype2] returns a float that describes how effective an
  * attack of type [ptype1] is against a pokemon of [ptype2].
