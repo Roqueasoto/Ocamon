11f0baace55a9dc67a51590f0fd358c4beb8c98f
vagrant <vagrant@vm3110-2018sp>
2018-04-30 00:45:07 -0400

    Created Testing Harness for Everyone

---
+++ Tim/test_maker.py
@@ -0,0 +1,36 @@
+def string_of_int(n):
+    str_n = str(n)
+    z = 3 - len(str_n)
+    return "0" * z + str_n
+
+def make_line1(n):
+    num = string_of_int(n)
+    return "let test"+num+"_name            = \"Timothy "+num+"\"\n"
+
+def make_line2(n):
+    num = string_of_int(n)
+    return "let test"+num+"_expression      = ()\n"
+
+def make_line3(n):
+    num = string_of_int(n)
+    return "let test"+num+"_expected_result = ()\n"
+
+def make_bottom_line(n):
+    num = string_of_int(n)
+    return "  test"+num+"_name >:: (fun _ -> assert_equal test"+num+"_expected_result test"+num+"_expression);\n"
+
+
+def make_top():
+    file = open("output.txt", "w")
+    for i in range(0, 101):
+        file.write(make_line1(i))
+        file.write(make_line2(i))
+        file.write(make_line3(i))
+        file.write("\n")
+
+def make_bottom():
+    file = open("output.txt", "w")
+    for i in range(0, 101):
+        file.write(make_bottom_line(i))
+
+make_top()
---
+++ test_andrea.mli
@@ -0,0 +1,4 @@
+(* DO NOT MODIFY THIS FILE. Put your test cases in test_andrea.ml *)
+
+(* Andrea's OUnit test cases. *)
+val tests : OUnit2.test list
