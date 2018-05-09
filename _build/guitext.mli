a3afcf9c1f59732b4dbbf9ded5c5fa1348fc1760
Roque <ras656@cornell.edu>
2018-05-09 00:36:07 -0400

    Fix compilation errors in ai.ml'

---
+++ _build/guitext.mli
@@ -0,0 +1,10 @@
+open Controller
+open Shared_types
+
+(* [get_cmd gui_inf gmode] is the command that represents the user input based
+ * on the current mode [gmode] and relevant information about the state
+ * [gui_inf] as defined in the Types module.
+ * requires:
+ * - [gui_inf] are the state details for gui to use as defined in Types.
+ * - [gmode] is the current mode of the game corresponding to the state. *)
+val get_cmd : gui_info -> mode -> command
