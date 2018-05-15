33b8970da3c0cbedaa37ac6c06a9f81e5fe73746
vagrant <vagrant@vm3110-2018sp>
2018-05-15 06:19:57 -0400

    resolved

--- controller.ml
+++ controller.ml
@@ -6,12 +6,8 @@ type status = StatusNone | Sleep | Paralyze | Burn | Frozen | Poisoned | Toxic
 
 type ptype = Normal | Fire | Water | Electric | Grass | Ice | Fighting
            | Poison | Ground | Flying | Psychic | Bug | Rock | Ghost | Dragon
-<<<<<<< HEAD
 
 type category = SpecialA | Physical
-=======
-type category = Special | Physical
->>>>>>> 48493085429fa4fe36fe84451f2bc6a31b22f925
 
 type bufftype =
     ATKBuff of int | DEFBuff of int | SPDBuff of int | SpatkBuff of int
