829f629b269022934262a6eebb6f27c246110c8f
vagrant <vagrant@vm3110-2018sp>
2018-05-15 03:32:08 -0400

    Implement Fainting switching and preventing fainted pokemon from acting.

---
+++ _build/controller.ml
@@ -0,0 +1,35 @@
+type effect_on = Self | Other
+
+type status = StatusNone | Sleep | Paralyze | Burn | Frozen | Poisoned | Toxic
+            | Confused | Flinch | Substitute | Uncontrollable | Focused
+            | LeechSeed | Mist
+
+type ptype = Normal | Fire | Water | Electric | Grass | Ice | Fighting
+           | Poison | Ground | Flying | Psychic | Bug | Rock | Ghost | Dragon
+
+type category = Special | Physical
+
+type bufftype =
+    ATKBuff of int | DEFBuff of int | SPDBuff of int | SpatkBuff of int
+
+type choices = CStart of int | CMap | CBattleEnd | CWin | CLose of bool
+             | CWinGame of bool | CQuit
+
+type special =
+  | HealStatus of status
+  | Revive
+
+type effect =
+  | Switch of int
+  | Heal of effect_on    * int * int
+  | Damage of effect_on  * int * int * (int * int) * ptype * category
+  | Status of effect_on  * int * status
+  | Buff of effect_on    * int * bufftype
+  | Special of effect_on * int * special * effect
+  | Nothing
+
+type command =
+  | Move of string
+  | Interact of choices
+  | CombatAction of effect list
+  | Round of effect list * effect list
